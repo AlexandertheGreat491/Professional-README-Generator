329643c7ca36a3b70377724ab2e5925ce325825d
agvan <agvandyke25@gmail.com>
2022-06-22 16:11:34 -0500

    wrote generateMarkdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +15,26 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description:
+${data.description}
 
+## Table of Contents
+-[Installation](#installation)
+-[Usage](#usage)
+-[Credits](#credits)
+-[License](#license)
+
+## Installation:
+${data.installation}
+
+## Usage:
+${data.usage}
+
+## Credts
+${data.credits}
+
+## License
+${data.license}
 `;
 }
 
