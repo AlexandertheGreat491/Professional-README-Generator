372ad70d65614823aa21df06c163c18aa31936bd
agvan <agvandyke25@gmail.com>
2022-06-23 18:55:28 -0500

    deleted commented out code & updated comments to accurately describe the code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//Function returns a license.
+
 function renderLicense(license){
   
   if (license === 'Apache') {
@@ -13,44 +13,9 @@ function renderLicense(license){
     return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)(https://www.gnu.org/licenses/gpl-3.0)'
   }
 }
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-//function renderLicenseLink(license) {
-  // if license is chosen return the link to the text file of the license:
-  //if(element){
-    //return '[Link to the Licence](../src/' + encodeURIComponent(element) + '.txt)'
-  //}else{
-  // else return empty string
-    //return '';
-  //}
-  
-//}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-//function renderLicenseSection(license) {
-  //let licenceText = '';
-  //license.forEach(element => {
-    //licenceText +=  element + ': <br />';
-    //licenceText += renderLicenseLink(element) + '<br />';
-    //switch(element){
-      //case 'MIT License':
-        //licenceText += 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available \n';
-        //break;
-      //case 'ISC':
-        //licenceText += 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. \n';
-        //break;
-      //default:
-        //licenceText +='';
-    //}
-    //licenceText += '<br /><br />';
-  //});
-  //return licenceText;
-
-//}
 
 
-// TODO: Create a function to generate markdown for README
+//Function generates the markdown for the README.
 function generateMarkdown(data) {
   return `# ${data.title}
   
